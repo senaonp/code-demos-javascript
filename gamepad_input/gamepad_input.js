073c16b96f7a0bc16d0ae67581e781d9f7c65b22
Tofuni <fran@fbacarisas.xyz>
2021-02-23 19:30:22 -0500

    handle multiple controllers (display last plugged in gamepad); reset listener when controller is unplugged

--- gamepad_input/gamepad_input.js
+++ gamepad_input/gamepad_input.js
@@ -5,6 +5,9 @@ var refreshRate = 50; // get state of gamepad 20 times per second
 var cleanInputsRate = 3000; // check inputsList every 3 seconds
 var maxInputs = 200; // threshold of items to have in inputsList
 var gp = {}; // a gamepad object when initialized
+var controllerNum = 0;
+var inputInterval = 0;
+var clearInputInverval = 0;
 var buttonMapping = {'0':'🞩', '1':'○', '2':'□', '3':'△', '4':'L1', '5':'R1', '6':'L2', '7':'R2', '8':'i', '9':'j',
 	'10':'k', '11':'l', '12':'⇧', '13':'⇩', '14':'⇦', '15':'⇨', '16':'q', '17':'r', '18':'s',} // button mapping based on Mayflash Arcade Stick controller
 
@@ -14,13 +17,16 @@ var inputsListElem = document.getElementById("inputSequence");
 
 window.addEventListener("gamepadconnected", function(e) {
 	var mygamepad = e.gamepad;
-	setInterval(setInput, refreshRate);
-	setInterval(clearInputs, cleanInputsRate);
-	gamepadStatus.innerText = "ON";
+	controllerNum = mygamepad.index;
+	inputInterval = setInterval(setInput, refreshRate);
+	clearInputInverval = setInterval(clearInputs, cleanInputsRate);
+	gamepadStatus.innerText = "ON - [" + mygamepad.id +"]  - (Controller_" + (controllerNum+1) + ")";
 });
 
 window.addEventListener("gamepaddisconnected", function(e) {
 	gamepadStatus.innerText = "OFF";
+	clearInterval(inputInterval);
+	clearInterval(clearInputInverval);
 });
 
 function setInput() {
@@ -32,7 +38,7 @@ function setInput() {
 }
 
 function getButtonState() {
-	gp = navigator.getGamepads()[0];
+	gp = navigator.getGamepads()[controllerNum];
 	activeButtons = [];
 	gp.buttons.forEach(function(button, index, array) {
 		if (button.pressed) {
